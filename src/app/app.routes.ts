2f53d479647e4d9c4176c0706be6a540e8785106
David Merino <dmerino@lksnext.com>
2025-06-22 17:37:35 +0200

    cambio de master a main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Componente1 } from './components/componente1/componente1';
+import { Componente2 } from './components/componente2/componente2';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Componente1
+  },
+  {
+    path: 'pagina2',
+    component: Componente2
+  }
+];
